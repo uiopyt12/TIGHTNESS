3f4c516e73c0905249c63677dc36b359ac2cc862
uiopyt12 <108012256+uiopyt12@users.noreply.github.com>
2023-11-24 23:39:16 +0900

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,25 @@
+﻿#include <SFML/Graphics.hpp>
+using namespace sf;
+
+
+int main()
+{
+    RenderWindow window(sf::VideoMode(600, 800), "TIGHTNESS", sf::Style::None);
+
+
+    while (window.isOpen())
+    {
+        window.display();
+
+        Event e;
+        while (window.pollEvent(e))
+        {
+            if (e.type == Event::Closed)
+                window.close();
+        }
+
+
+    }
+
+    return 0;
+}
