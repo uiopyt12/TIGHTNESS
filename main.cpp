664ed309e7e4ea290a7bd5c71167ea5a4af0d28c
uiopyt12 <108012256+uiopyt12@users.noreply.github.com>
2023-12-28 13:00:11 +0900

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,25 +1,46 @@
 ﻿#include <SFML/Graphics.hpp>
+#include <iostream>
 using namespace sf;
+using namespace std;
+#define WIDTH 600
+#define HEIGHT 800
+
+
 
 
 int main()
 {
-    RenderWindow window(sf::VideoMode(600, 800), "TIGHTNESS", sf::Style::None);
+    RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "TIGHTNESS");
 
 
     while (window.isOpen())
     {
         window.display();
-
         Event e;
+
         while (window.pollEvent(e))
         {
             if (e.type == Event::Closed)
                 window.close();
-        }
+
+            if (e.type == sf::Event::MouseEntered)
+                cout << "the mouse cursor has entered the window" << std::endl;
+
+            if (e.type == sf::Event::MouseLeft)
+                cout << "the mouse cursor has left the window" << std::endl;
 
 
-    }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::C))
+            {
+                cout << "hi" << endl;
+            }
+            
+        }//window.pollEvent
+
+
+    } //window.isOpen
+
+
 
     return 0;
 }
